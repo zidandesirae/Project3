438534dfbc51e395909c6661159e372f6446b1ee
River <jc85233@yahoo.com>
2020-04-19 13:07:19 -0700

    making changes to app.css

--- project3reactapp/src/App.js
+++ project3reactapp/src/App.js
@@ -3,6 +3,7 @@ import Login from './components/PageBuild/Login';
 // import SignUp from './components/pageBuild/SignUp';
 // import Main from './components/pageBuild/Main';
 // import Settings from "./components/pageBuild/Settings";
+import "./components/App.css"
 
 function App() {
   return (
