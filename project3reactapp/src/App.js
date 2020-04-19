8ad41efb32280fe49872e33c1567593dbd5ef3db
River <jc85233@yahoo.com>
2020-04-19 12:29:40 -0700

    login functional

--- project3reactapp/src/App.js
+++ project3reactapp/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-// import Login from './components/pages/Login';
+import Login from './components/pages/Login';
 // import SignUp from './components/pages/SignUp';
 // import Main from './components/pages/Main';
 // import Settings from "./components/pages/Settings";
 
 function App() {
   return (
-    INSERT
+    <Login/>
   );
 }
 
