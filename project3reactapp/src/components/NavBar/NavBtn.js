c555be6f02bad9603b284df84ad75a16344a09e5
Desirae Zidan <zidan.desirae@gmail.com>
2020-04-20 12:09:30 -0700

    minor change

--- project3reactapp/src/components/NavBar/NavBtn.js
+++ project3reactapp/src/components/NavBar/NavBtn.js
@@ -8,29 +8,25 @@ const navbtns = [
     {
         id: 1,
         title: "Home",
-        route: <Main />
     },
     {
         id: 2,
         title: "Settings",
-        route: <Settings />
     },
     {   
         id: 3,
         title: "Group Information",
-        route: ""
     },
     {
         id: 4,
         title: "Log Out",
-        route: <Login />
     }
 ];
 
 function NavBtn(props) {
     return (
         navbtns.map(btn => (
-            <a className="nav-item nav-link" href="#" onClick={btn.route} key={btn.id}>{btn.title}</a>
+            <a className="nav-item nav-link" href="#" key={btn.id}>{btn.title}</a>
         ))
     );
 }
