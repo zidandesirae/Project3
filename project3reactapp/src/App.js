aa00a1ff8171ea5fe66a8aa17648c961eb795540
Christopher Matthews <sirhunty@gmail.com>
2020-04-20 13:32:43 -0700

    added snycfusion to test a calendar option

--- project3reactapp/src/App.js
+++ project3reactapp/src/App.js
@@ -3,6 +3,13 @@ import React from 'react';
 // import SignUp from './components/PageBuild/SignUp';
 import Main from './components/PageBuild/Main';
 // import Settings from "./components/PageBuild/Settings";
+import { ScheduleComponent } from '@syncfusion/ej2-react-schedule';
+
+class App extends React.Component {
+  public render () {
+    return <ScheduleComponent> </ScheduleComponent>
+  }
+}
 
 function App() {
   return (
