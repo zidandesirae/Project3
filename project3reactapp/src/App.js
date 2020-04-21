44f24091099c87a4c657c793f484e081ee2d28f9
Hunter Matthews <sirhunty@gmail.com>
2020-04-21 15:45:18 -0700

    udating my brach with current test

--- project3reactapp/src/App.js
+++ project3reactapp/src/App.js
@@ -1,15 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
-// import Login from './components/PageBuild/Login';
-// import SignUp from './components/PageBuild/SignUp';
-// import Main from './components/PageBuild/Main';
-// import Settings from "./components/PageBuild/Settings";
-import CalendarShare from "./components/PageBuild/CalendarShare";
-
-function App() {
-  return (
-    <CalendarShare />
-=======
 // import Login from './pages/Login';
 // import SignUp from './pages/SignUp';
 // import Main from './pages/Main';
@@ -19,7 +8,6 @@ import GroupInfo from './pages/GroupInfo';
 function App() {
   return (
     <GroupInfo />
->>>>>>> da1c638f7093080b34e77ce17f3fdad504a5944d
   );
 }
 
