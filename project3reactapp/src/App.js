16201db825c4653989814e894ecd35298b937053
River <jc85233@yahoo.com>
2020-04-23 11:56:29 -0700

    chenages to routes

--- project3reactapp/src/App.js
+++ project3reactapp/src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 // import Login from './components/PageBuild/Login';
 // import SignUp from './components/PageBuild/SignUp';
 import Main from './components/PageBuild/Main';
+import Calendar from './components/PageBuild/Calendar';
 // import Settings from "./components/PageBuild/Settings";
 // import Calendar from "./components/PageBuild/Calendar";
 function App() {
   return (
-    <Main />
+    <Calendar />
   );
 }
 
