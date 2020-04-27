6e0d5a3088c5806ec0709adc60a1f2aa6c7a1199
River <jc85233@yahoo.com>
2020-04-27 16:30:35 -0700

    css on navbar and background

--- project3reactapp/src/components/Universal/NavBar.js
+++ project3reactapp/src/components/Universal/NavBar.js
@@ -6,7 +6,7 @@ const appTitle = "Our Circle";
 
 function NavBar(props) {
     return (
-        <nav className="navbar navbar-expand-xs navbar-light bg-light">
+        <nav className="navbar navbar-expand-xs navbar-light">
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarMarkup" aria-controls="navbarMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
