0470a1cd0a4f4a46e6d8cfa133eb9a8a783bbf87
River <jc85233@yahoo.com>
2020-04-27 16:17:49 -0700

    logos and boarders

--- project3reactapp/src/components/Login/LoginPage.js
+++ project3reactapp/src/components/Login/LoginPage.js
@@ -1,11 +1,13 @@
 import React from "react";
 import TitleH1 from "../Universal/TitleH1";
+import OurCircle from "../Images/OurCircle.png";
 
 function LoginPage(props) {
     return (
         <div className="container my-4 py-4">
             <div className="row">
                 <div className="col-md-8 mx-auto text-center">
+                <img src={OurCircle} className="signUpLogo" alt="image1" />
                     <TitleH1 title="Our Circle" subtitle="Always in the Loop" />
                     <div className="card">
                         <div className="card-body">
