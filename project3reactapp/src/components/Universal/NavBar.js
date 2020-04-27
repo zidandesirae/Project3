b05d9cf6098c2ab77223b230f2438d5c6e4f4dc3
River <jc85233@yahoo.com>
2020-04-27 12:38:09 -0700

    addint coloring to borders

--- project3reactapp/src/components/Universal/NavBar.js
+++ project3reactapp/src/components/Universal/NavBar.js
@@ -10,7 +10,6 @@ function NavBar(props) {
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarMarkup" aria-controls="navbarMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
-            {/* <a className="navbar-brand" href="/home">{OurCircle}</a> */}
             <img src={OurCircle} className="logo" alt="image1"/>
             <div className="collapse navbar-collapse" id="navbarMarkup">
                 <div className="navbar-nav mr-auto mt-2 mt-lg-0">
