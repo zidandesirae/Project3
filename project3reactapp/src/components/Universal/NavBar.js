0be6f7b9bf457968ef38e6b6b7a2d3d20bc49158
River <jc85233@yahoo.com>
2020-04-27 09:49:42 -0700

    adding image npm

--- project3reactapp/src/components/Universal/NavBar.js
+++ project3reactapp/src/components/Universal/NavBar.js
@@ -10,7 +10,9 @@ function NavBar(props) {
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarMarkup" aria-controls="navbarMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
-            <a className="navbar-brand" href="/home">{OurCircle}</a>
+            {/* <a className="navbar-brand" href="/home">{OurCircle}</a> */}
+            <img src={OurCircle} className="card-img-top" alt="image1"/>
+                <Transformation height="50" width="50" crop="fill" effect="sepia" radius="20" />
             <div className="collapse navbar-collapse" id="navbarMarkup">
                 <div className="navbar-nav mr-auto mt-2 mt-lg-0">
                     <NavBtns />
