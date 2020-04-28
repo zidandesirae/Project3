36c6fc337eff0e53d532c4e35f1839320cbca324
River <jc85233@yahoo.com>
2020-04-28 13:30:42 -0700

    adding effects in css

--- project3reactapp/src/pages/Main.js
+++ project3reactapp/src/pages/Main.js
@@ -4,14 +4,14 @@ import MainSeg from '../components/Main/MainSeg';
 
 function Main(props) {
     return (<div>
-        <div id="containerHead">
-  Stay 
+        <div className="bodyTextScroll" id="containerHead">
+            Stay
   <div id="flip">
-        <div><div>organized</div></div>
-        <div><div>in the loop</div></div>
-        <div><div>connected</div></div>
-    </div>
-    </div>
+                <div><div>organized</div></div>
+                <div><div>in the loop</div></div>
+                <div><div>connected</div></div>
+            </div>
+        </div>
         <Container2>
             <MainSeg />
         </Container2>
