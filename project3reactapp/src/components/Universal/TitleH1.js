0be433ecbd8cc129472983f0ba71e402b24f2f29
River <jc85233@yahoo.com>
2020-04-28 14:47:42 -0700

    adding title css and shows effects

--- project3reactapp/src/components/Universal/TitleH1.js
+++ project3reactapp/src/components/Universal/TitleH1.js
@@ -3,7 +3,7 @@ import React from "react";
 function TitleH1(props) {
     return (
         <div className="mb'-4">
-            <h1 className="display-2">{props.title}</h1>
+            <h1 className="display-2 text-3d" >{props.title}</h1>
             <h4>{props.subtitle}</h4>
         </div>
     );
