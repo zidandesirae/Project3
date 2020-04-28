0d40cab1517446e0699605a72b643dc714fc3056
River <jc85233@yahoo.com>
2020-04-27 20:05:30 -0700

    added cdd for rolling text to home page

--- project3reactapp/src/pages/Main.js
+++ project3reactapp/src/pages/Main.js
@@ -3,10 +3,14 @@ import Container2 from '../components/Universal/Container2';
 import MainSeg from '../components/Main/MainSeg';
 
 function Main(props) {
-    return (<div><div id="flip">
-        <div><div>life</div></div>
-        <div><div>family</div></div>
-        <div><div>friends</div></div>
+    return (<div>
+        <div id="containerHead">
+  Stay 
+  <div id="flip">
+        <div><div>organized</div></div>
+        <div><div>in the loop</div></div>
+        <div><div>connected</div></div>
+    </div>
     </div>
         <Container2>
             <MainSeg />
