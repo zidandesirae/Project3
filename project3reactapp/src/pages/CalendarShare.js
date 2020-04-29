7916224da69f8fcab7db083a48b5aa160777b3d6
Christopher Matthews <sirhunty@gmail.com>
2020-04-29 16:00:01 -0700

    updating my branch to merge with test

--- project3reactapp/src/pages/CalendarShare.js
+++ project3reactapp/src/pages/CalendarShare.js
@@ -13,6 +13,7 @@ import "react-big-calendar/lib/css/react-big-calendar.css";
 const localizer = momentLocalizer(moment);
 
 class CalendarShare extends Component {
+    // set to event state; 
     state = {
         events: [
             {
