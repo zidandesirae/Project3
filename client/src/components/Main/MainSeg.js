de53c674072e3446599bbef4022c7d184a5b1c12
River <jc85233@yahoo.com>
2020-04-30 21:00:18 -0700

    icons for dash

--- client/src/components/Main/MainSeg.js
+++ client/src/components/Main/MainSeg.js
@@ -7,25 +7,36 @@ const eachSeg = [
         id: 1,
         img: "https://lakelandescaperoom.com/wp-content/uploads/2016/09/image-placeholder-500x500.jpg",
         alt: "calendar",
-        route: '/calendar'
+        route: '/calendar',
+        className: "far fa-calendar-alt text-3d-iconSizing",
+        heading: "Calendar"
     },
     {
         id: 2,
         img: "https://lakelandescaperoom.com/wp-content/uploads/2016/09/image-placeholder-500x500.jpg",
         alt: "dashboard",
-        route: '/dashboard'
+        route: '/dashboard',
+        className: "far fa-comment-dots text-3d-iconSizing",
+        heading: "Chat"
+
     },
     {
         id: 3,
         img: "https://lakelandescaperoom.com/wp-content/uploads/2016/09/image-placeholder-500x500.jpg",
         alt: "lists",
-        route: '/lists'
+        route: '/lists',
+        className: "fas fa-list-ol text-3d-iconSizing",
+        heading: "Lists"
+
     },
     {
         id: 4,
         img: "https://lakelandescaperoom.com/wp-content/uploads/2016/09/image-placeholder-500x500.jpg",
         alt: "photos",
-        route: '/photoalbum'
+        route: '/photoalbum',
+        className: "far fa-image text-3d-iconSizing",
+        heading: "Photos"
+
     }
 ];
 
@@ -35,8 +46,9 @@ function MainSeg(props) {
             <div class="col-6 p-4">
             <Link to={item.route}>
                 <div className="card">
-                    <div className="card-body">
-                        <img src={item.img} alt={item.alt} key={item.id} className="img-fluid" />
+                    <div className="card-body text-center">
+                        
+                        <i className={item.className}></i>
                     </div>
                 </div>
             </Link>
