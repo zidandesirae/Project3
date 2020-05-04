0206be80588a9de608ad311b34bed72db722333d
River <jc85233@yahoo.com>
2020-05-04 11:55:53 -0700

    adding states and hooks to file upload

--- client/src/components/Other/FileUpload.js
+++ client/src/components/Other/FileUpload.js
@@ -1,16 +1,24 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
 
 const FileUpload = () => {
+const [file, setFile] = useState('');
+const [filename, setFilename] = useState('Choose File');
+
+const onChange = e => {
+    setFile(e.target.files[0]);
+    setFilename(e.target.files[0].name);
+}
+
     return (
         <div>
 
             <Fragment>
                 <form>
                     <div className="custom-file">
-                        <input type="file" className="custome-file-input" id="customefile"/>
+                        <input type="file" className="custome-file-input" id="customefile" onChange={onChange}/>
                             <lable className="custome-file-lable" for="customFile">
-                                Chouse file
-                                </lable>
+                                {fileName}
+                            </lable>
                     </div>
                     <input type="submit" value="Uplaod" className="btn btn=primary btm-block mt-4"/>
                 </form>
