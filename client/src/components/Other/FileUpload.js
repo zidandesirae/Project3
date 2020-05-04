357d7668212753bdb3b9b57acac5d631670e76be
River <jc85233@yahoo.com>
2020-05-04 11:31:01 -0700

    form adding

--- client/src/components/Other/FileUpload.js
+++ client/src/components/Other/FileUpload.js
@@ -1,15 +1,20 @@
-import React, {Fragment} from 'react'
- 
+import React, { Fragment } from 'react'
+
 const FileUpload = () => {
     return (
         <div>
 
-<Fragment>
-    <form>
-
-    </form>
-    
-    </Fragment>/>
+            <Fragment>
+                <form>
+                    <div className="custom-file">
+                        <input type="file" className="custome-file-input" id="customefile"/>
+                            <lable className="custome-file-lable" for="customFile">
+                                Chouse file
+                                </lable>
+                    </div>
+                    <input type="submit" value="Uplaod" className="btn btn=primary btm-block mt-4"/>
+                </form>
+            </Fragment>/>
 
         </div>
     )
