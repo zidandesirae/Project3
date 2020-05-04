a3d2092fece5b105002a4ff2771681a9ba2ab55e
River <jc85233@yahoo.com>
2020-05-04 11:36:43 -0700

    adding fileupload to photoalbum

--- client/src/pages/PhotoAlbum.js
+++ client/src/pages/PhotoAlbum.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import Container2 from '../components/Universal/Container2';
 import TitleH2 from '../components/Universal/TitleH2';
 import Col10 from '../components/Universal/Col10';
-import NavBar from '../components/Universal/NavBar';
+import NavBar from '../components/Universal/NavBar'; 
+import FileUpload from '../components/Other/FileUpload';
 
 class PhotoAlbum extends Component {
     render() {
@@ -13,6 +14,7 @@ class PhotoAlbum extends Component {
                     <Col10>
                         <TitleH2 title="Photo Album" />
                         {<div>"INSERT CONTENT HERE"</div>}
+                        <FileUpload/>
                     </Col10>
                 </Container2>
             </div>
