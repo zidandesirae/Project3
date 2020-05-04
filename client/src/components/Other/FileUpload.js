32b04552aa5fba0073b0657939a702870d991e63
River <jc85233@yahoo.com>
2020-05-04 11:15:03 -0700

    adding form

---
+++ client/src/components/Other/FileUpload.js
@@ -0,0 +1,18 @@
+import React, {Fragment} from 'react'
+ 
+const FileUpload = () => {
+    return (
+        <div>
+
+<Fragment>
+    <form>
+
+    </form>
+    
+    </Fragment>/>
+
+        </div>
+    )
+}
+
+export default FileUpload
