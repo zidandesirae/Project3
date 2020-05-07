6f1b88260ce86cff20c2dba5e8465e3c30944946
Desirae Zidan <zidan.desirae@gmail.com>
2020-05-06 20:18:04 -0700

    delete

--- project3reactapp/src/components/Main/Col6.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function Col6(props) {
-    return (
-        <div class="col-6 p-4">{props.children}</div>
-    );
-}
-
-export default Col6;
--- project3reactapp/src/components/Settings/UserInfo.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function UserInfo(props) {
-    return (
-        <h5>INSERT</h5>
-    );
-}
-
-export default UserInfo;
