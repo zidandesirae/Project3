103717808a473672a032890ac05862416c471548
Desirae Zidan <zidan.desirae@gmail.com>
2020-06-03 15:35:09 -0700

    changed back

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ app.post('/upload', (req, res) => {
 app.use(routes);
 
 // Start the API server
-db.sequelize.sync({force: true}).then(function() {
+db.sequelize.sync({}).then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
